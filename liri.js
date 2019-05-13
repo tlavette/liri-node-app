df125bf95aa0c98639f28b49f4fd2189b8bd93c6
tlavette <tlavette@yahoo.com>
2019-05-13 06:22:05 -0400

    isolated log for just rotton tomatoes, pending clean up to display values only

--- liri.js
+++ liri.js
@@ -14,7 +14,7 @@ var selectSearch = process.argv[2];
 var value = process.argv[3];
 
 // I'm using switch-case statement
-switch(selectSearch) {
+switch (selectSearch) {
     case "concert-this":
         concertThis();
         break;
@@ -30,39 +30,38 @@ switch(selectSearch) {
 
 
 // functions defined for each to initially console log option.
-function concertThis(){
+function concertThis() {
     if (selectSearch === "concert-this") {
         console.log("This option will search bands in town!");
 
-    }    
+    }
 }
 
-function spotifyThisSong(){
-    if(selectSearch === "spotify-this-song"){
+function spotifyThisSong() {
+    if (selectSearch === "spotify-this-song") {
         console.log("This option will search for songs on Spotify!");
     }
 }
 
-function movieThis(){
-    if(selectSearch === "movie-this"){
+function movieThis() {
+    if (selectSearch === "movie-this") {
         axios.get("http://www.omdbapi.com/?apikey=trilogy&t=" + value).then(
-             function(response){
-                 console.log("The movie: " + response.data.Title);
-                 console.log("Year: " + response.data.Year);
-                 
+            function (response) {
+                console.log("The movie: " + response.data.Title);
+                console.log("Year: " + response.data.Year);
                 console.log("IMDB Rating: " + response.data.imdbRating);
-                console.log(response.data.Ratings);
-                 console.log("Production Country: " + response.data.Country);
-                 console.log("Lanugage: " + response.data.Language);
-                 console.log("Plot: " + response.data.Plot);
-                 console.log("Actors: " + response.data.Actors);
-             }
+                console.log(response.data.Ratings[1]);
+                console.log("Production Country: " + response.data.Country);
+                console.log("Lanugage: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+            }
         );
-             
-         }   
-    
-       
-        }
+
+    }
+
+
+}
 
 
 
